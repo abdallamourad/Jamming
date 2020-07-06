7b5cdead7f98d3d385355d0c2ae61789c32f4de0
AbdullahFawzy <abdullah.f98m@gmail.com>
2020-07-06 20:47:12 +0800

    Updates

---
+++ precache-manifest.94eaba176f093e1d5eafd2aca7e5c420.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c4a57df6a355b19eef7d973869ec64f",
+    "url": "/Jamming/index.html"
+  },
+  {
+    "revision": "ee8fa1ce54631a2860ec",
+    "url": "/Jamming/static/css/main.ad311386.chunk.css"
+  },
+  {
+    "revision": "107a972cb13f89d1e9c9",
+    "url": "/Jamming/static/js/2.ddb409a7.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/Jamming/static/js/2.ddb409a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee8fa1ce54631a2860ec",
+    "url": "/Jamming/static/js/main.fa29c8a1.chunk.js"
+  },
+  {
+    "revision": "3b2b0cc45df1a01e0701",
+    "url": "/Jamming/static/js/runtime-main.303a8432.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/Jamming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
