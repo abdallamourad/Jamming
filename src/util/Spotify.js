47eac4c01c481f59a86fbefec3c78955728f7340
AbdullahFawzy <abdullah.f98m@gmail.com>
2020-07-06 23:13:38 +0800

    edit track style

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,14 +1,13 @@
-
 const clientID = 'c31c7f011b4644a49b5ff29df3089489';
 const redirectURI = 'https://abdullahfawzy.github.io/Jamming/';
 
-let accessToken;
+var accessToken;
 
 const Spotify = {
     getAccessToken() {
         if (accessToken) {
             return accessToken;
-        } 
+        }
 
         // check for access token
         const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
@@ -30,26 +29,26 @@ const Spotify = {
 
     search(term) {
         const accessToken = Spotify.getAccessToken();
-        
-        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, { 
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            }
-        })
-        .then(response => response.json())
-        .then(jsonResponse => {
-            if (!jsonResponse.tracks) {
-                return [];
-            }
-            
-            return jsonResponse.tracks.items.map(track => ({
+
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            })
+            .then(response => response.json())
+            .then(jsonResponse => {
+                if (!jsonResponse.tracks) {
+                    return [];
+                }
+
+                return jsonResponse.tracks.items.map(track => ({
                     id: track.id,
                     name: track.name,
                     artist: track.artists[0].name,
                     album: track.album.name,
                     uri: track.uri
-            }))
-        });
+                }))
+            });
     },
 
     savePlaylist(name, trackUris) {
@@ -61,21 +60,18 @@ const Spotify = {
         const headers = { Authorization: `Bearer ${accessToken}` };
         let userId;
 
-        return fetch('https://api.spotify.com/v1/me', { headers: headers }
-        ).then(response => response.json()
-        ).then(jsonResponse => {
+        return fetch('https://api.spotify.com/v1/me', { headers: headers }).then(response => response.json()).then(jsonResponse => {
             userId = jsonResponse.id;
             return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
                 headers: headers,
                 method: 'POST',
                 body: JSON.stringify({ name: name })
-            }).then(response => response.json()
-            ).then(jsonResponse => {
+            }).then(response => response.json()).then(jsonResponse => {
                 const playlistId = jsonResponse.id;
                 return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
                     headers: headers,
                     method: 'POST',
-                    body: JSON.stringify({ uris: trackUris })                    
+                    body: JSON.stringify({ uris: trackUris })
                 })
             });
         });
